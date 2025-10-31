e902d6b11e7be5bbccf75c666f90c981ce8930c9
Memphis Herghelegiu <memphispc70@gmail.com>
2025-10-31 09:09:01 +0100

    Backup

---
+++ backend/src/main/kotlin/com/csw/turtleapi/api/v1/config/WebConfig.kt
@@ -0,0 +1,20 @@
+package com.csw.turtleapi.api.v1.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.method.HandlerTypePredicate
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+
+//    override fun configurePathMatch(configurer: PathMatchConfigurer) {
+//        val predicate = HandlerTypePredicate
+//            .forBasePackage("com.csw.turtleapi.api.v1")
+//            .and { HandlerTypePredicate.forAnnotation(RestController::class.java) }
+//
+//        configurer.addPathPrefix("/api/v1", predicate)
+//    }
+
+}
--- backend/src/main/kotlin/com/csw/turtleapi/api/v1/controller/UserController.kt
+++ backend/src/main/kotlin/com/csw/turtleapi/api/v1/controller/UserController.kt
@@ -2,6 +2,7 @@ package com.csw.turtleapi.api.v1.controller
 
 import com.csw.turtleapi.api.v1.entity.User
 import com.csw.turtleapi.api.v1.exception.UserNotFoundException
+import com.csw.turtleapi.api.v1.exception.UsernameAlreadyExistsException
 import com.csw.turtleapi.api.v1.repository.UserRepository
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
@@ -15,28 +16,40 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/v1/users")
 class UserController(
-    private val userRepository: UserRepository
+    private val repository: UserRepository
 ) {
 
     @GetMapping
     fun getAll(): ResponseEntity<Set<User>> {
-        val result = userRepository.findAll().toSet()
-        return ResponseEntity.ok(result)
+        val users = repository.findAll().toSet()
+        return ResponseEntity.ok(users)
     }
 
     @GetMapping("/{username}")
     fun getByUsername(@PathVariable username: String): ResponseEntity<User> {
-        val result = userRepository
+        val user = repository
             .findByUsername(username)
             .orElseThrow { UserNotFoundException(username) }
 
-        return ResponseEntity.ok(result)
+        return ResponseEntity.ok(user)
     }
 
     @PostMapping
-    fun create(@RequestBody user: User): User? = userRepository.save(user)
+    fun create(@RequestBody user: User): ResponseEntity<User> {
+        repository.findByUsername(user.username)
+            .ifPresent { UsernameAlreadyExistsException(user.username) }
+
+        repository.save(user)
+    }
+
 
     @DeleteMapping("/{username}")
-    fun deleteByUsername(@PathVariable username: String) = userRepository.deleteByUsername(username)
+    fun deleteByUsername(@PathVariable username: String) {
+        val user = repository
+            .findByUsername(username)
+            .orElseThrow { UserNotFoundException(username) }
+
+        repository.delete(user)
+    }
 
 }
