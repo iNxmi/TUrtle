dcfa02e5e87f3e61915a1233bccd851bc205f88c
Memphis Herghelegiu <memphispc70@gmail.com>
2025-11-11 14:30:22 +0100

    made login, register and logout work

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -1,53 +1,65 @@
 <script>
-    import {Button, Checkbox, Label, Input, Heading, A, P} from "flowbite-svelte";
+    import {Button, Checkbox, Label, Input, Heading, A, P, Modal} from "flowbite-svelte";
     import {m} from '$lib/paraglide/messages.js';
 
     let apiResponse = $state(null);
+    let modal = $state(false);
 
     async function login(event) {
         event.preventDefault();
 
-        const username = document.getElementById('usernameOrEmail').value;
+        const username = document.getElementById('username_or_email').value;
         const password = document.getElementById('password').value;
+
+        const payload = {
+            username: username,
+            password: password
+        }
+
         const response = await fetch('/api/auth/login', {
             method: 'POST',
-            body: JSON.stringify({username: username, password: password}),
-            headers: {
-                'Content-Type': 'application/json'
-            }
+            body: JSON.stringify(payload),
+            headers: {'Content-Type': 'application/json'}
         });
 
         apiResponse = await response.json();
+        modal = true;
     }
 </script>
 
-<form class="flex flex-col gap-5" onsubmit={login}>
-    <Heading tag="h3" class="text-center">{m.login__title()}</Heading>
+<div>
+    <form class="flex flex-col gap-5" onsubmit={login}>
+        <Heading tag="h3" class="text-center">{m.login__title()}</Heading>
 
-    <Label>
-        <span>{m.login__username_or_email_label()}</span>
-        <Input name="username_or_email" type="text" required/>
-    </Label>
+        <Label>
+            <span>{m.login__username_or_email_label()}</span>
+            <Input id="username_or_email" type="text" required/>
+        </Label>
 
-    <Label>
-        <span>{m.login__password_label()}</span>
-        <Input name="password" type="password" required/>
-    </Label>
+        <Label>
+            <span>{m.login__password_label()}</span>
+            <Input id="password" type="password" required/>
+        </Label>
 
-    <Checkbox>{m.login__remember_me({days: 30})}</Checkbox>
+        <Checkbox>{m.login__remember_me({days: 30})}</Checkbox>
 
-    <div class="border border-dashed">
-        <P class="text-center m-8">I am not a Robot ✅</P>
-    </div>
+        <div class="border border-dashed">
+            <P class="text-center m-8">I am not a Robot ✅</P>
+        </div>
 
-    <Button type="submit">{m.login__button()}</Button>
+        <Button type="submit">{m.login__button()}</Button>
 
-    <div class="flex gap-5 justify-between">
-        <A href="/register" class="text-sm text-blue-700 hover:underline dark:text-blue-500">{m.login__no_account()}</A>
-        <A href="/" class="text-sm text-blue-700 hover:underline dark:text-blue-500">{m.login__forgot_password()}</A>
-    </div>
-</form>
+        <div class="flex gap-5 justify-between">
+            <A href="/register"
+               class="text-sm text-blue-700 hover:underline dark:text-blue-500">{m.login__no_account()}</A>
+            <A href="/"
+               class="text-sm text-blue-700 hover:underline dark:text-blue-500">{m.login__forgot_password()}</A>
+        </div>
+    </form>
 
-{#if apiResponse}
-    <p class="whitespace-pre">{JSON.stringify(apiResponse, null, 2)}</p>
-{/if}
+    <Modal title="API Response" bind:open={modal}>
+        {#if apiResponse}
+            <P class="whitespace-pre">{JSON.stringify(apiResponse, null, 2)}</P>
+        {/if}
+    </Modal>
+</div>
--- frontend/src/routes/register/+page.svelte
+++ frontend/src/routes/register/+page.svelte
@@ -1,78 +1,91 @@
 <script>
-    import {Button, Checkbox, Label, Input, Heading, P, A} from "flowbite-svelte";
+    import {Button, Checkbox, Label, Input, Heading, A, P, Modal} from "flowbite-svelte";
     import {m} from '$lib/paraglide/messages.js';
 
     let apiResponse = $state(null);
+    let modal = $state(false);
 
-    async function login(event) {
+    async function register(event) {
         event.preventDefault();
 
-        const username = document.getElementById("username").value;
-        const password = document.getElementById("password").value;
+        const payload = {
+            username:  document.getElementById('username').value,
+            firstName: document.getElementById('first_name').value,
+            lastName: document.getElementById('last_name').value,
+            email: document.getElementById('email').value,
+            studentId: document.getElementById('student_id').value,
+            password: document.getElementById('password').value
+        };
+
+        alert(JSON.stringify(payload, null, 2));
+
         const response = await fetch('/api/auth/register', {
             method: 'POST',
-            body: JSON.stringify({username: username, password: password}),
-            headers: {
-                'Content-Type': 'application/json'
-            }
+            body: JSON.stringify(payload),
+            headers: {'Content-Type': 'application/json'}
         });
 
         apiResponse = await response.json();
+        modal = true;
     }
 </script>
 
-<form class="flex flex-col gap-5" onsubmit={login}>
-    <Heading tag="h3" class="text-center">{m.register__title()}</Heading>
+<div>
+    <form class="flex flex-col gap-5" onsubmit={register}>
+        <Heading tag="h3" class="text-center">{m.register__title()}</Heading>
 
-    <Label>
-        <span>{m.register__username_label()}</span>
-        <Input name="username" type="text" required/>
-    </Label>
-
-    <div class="flex gap-5">
-        <Label class="flex-1">
-            <span>{m.register__first_name_label()}</span>
-            <Input name="first_name" type="text" required/>
+        <Label>
+            <span>{m.register__username_label()}</span>
+            <Input id="username" type="text" required/>
         </Label>
-        <Label class="flex-1">
-            <span>{m.register__last_name_label()}</span>
-            <Input name="last_name" type="text" required/>
+
+        <div class="flex gap-5">
+            <Label class="flex-1">
+                <span>{m.register__first_name_label()}</span>
+                <Input id="first_name" type="text" required/>
+            </Label>
+            <Label class="flex-1">
+                <span>{m.register__last_name_label()}</span>
+                <Input id="last_name" type="text" required/>
+            </Label>
+        </div>
+
+        <Label>
+            <span>{m.register__email_label()}</span>
+            <Input id="email" type="email" required/>
         </Label>
-    </div>
 
-    <Label>
-        <span>{m.register__email_label()}</span>
-        <Input name="email" type="email" required/>
-    </Label>
-
-    <Label>
-        <span>{m.register__student_id_label()}</span>
-        <Input name="student_id" type="text" required/>
-    </Label>
-
-    <div class="flex gap-5">
-        <Label class="flex-1">
-            <span>{m.register__password_label()}</span>
-            <Input name="password" type="password" required/>
+        <Label>
+            <span>{m.register__student_id_label()}</span>
+            <Input id="student_id" type="text" required/>
         </Label>
-        <Label class="flex-1">
-            <span>{m.register__password_repeat_label()}</span>
-            <Input name="password_repeat" type="password" required/>
-        </Label>
-    </div>
 
-    <Checkbox>{m.register__i_agree_to_tos()}</Checkbox>
+        <div class="flex gap-5">
+            <Label class="flex-1">
+                <span>{m.register__password_label()}</span>
+                <Input id="password" type="password" required/>
+            </Label>
+            <Label class="flex-1">
+                <span>{m.register__password_repeat_label()}</span>
+                <Input id="password_repeat" type="password" required/>
+            </Label>
+        </div>
 
-    <div class="border border-dashed">
-        <h1 class="text-center m-8">I am not a Robot ✅</h1>
-    </div>
+        <Checkbox id="agree_tos">{m.register__i_agree_to_tos()}</Checkbox>
 
-    <Button type="submit" class="w-full1">{m.register__button()}</Button>
+        <div class="border border-dashed">
+            <h1 class="text-center m-8">I am not a Robot ✅</h1>
+        </div>
 
-    <A href="/login"
-       class="text-sm text-blue-700 hover:underline dark:text-blue-500">{m.register__already_have_a_account()}</A>
-</form>
+        <Button type="submit" class="w-full1">{m.register__button()}</Button>
 
-{#if apiResponse}
-    <p class="whitespace-pre">{JSON.stringify(apiResponse, null, 2)}</p>
-{/if}
+        <A href="/login"
+           class="text-sm text-blue-700 hover:underline dark:text-blue-500">{m.register__already_have_a_account()}</A>
+    </form>
+
+    <Modal title="API Response" bind:open={modal}>
+        {#if apiResponse}
+            <P class="whitespace-pre">{JSON.stringify(apiResponse, null, 2)}</P>
+        {/if}
+    </Modal>
+</div>
