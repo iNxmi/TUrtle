ced088975f3f4052fba26887cdeafdc68ea67637
Sanel <salihovicsanel@outlook.com>
2025-12-05 18:19:54 +0100

    Implemented DTO Files for RoomBooking

---
+++ backend/src/main/kotlin/de/csw/turtle/api/controller/api/RoomBookingController.kt
@@ -0,0 +1,36 @@
+package de.csw.turtle.api.controller.api
+
+import de.csw.turtle.api.Permission
+import de.csw.turtle.api.Permission.*
+import de.csw.turtle.api.controller.CRUDCreateController
+import de.csw.turtle.api.controller.CRUDDeleteController
+import de.csw.turtle.api.controller.CRUDGetController
+import de.csw.turtle.api.controller.CRUDPatchController
+import de.csw.turtle.api.dto.create.CreateRoomBookingRequest
+import de.csw.turtle.api.dto.get.GetRoomBookingResponse
+import de.csw.turtle.api.dto.patch.PatchRoomBookingRequest
+import de.csw.turtle.api.entity.RoomBookingEntity
+import de.csw.turtle.api.mapper.RoomBookingMapper
+import de.csw.turtle.api.service.RoomBookingService
+import de.csw.turtle.api.service.SecurityService
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/roombookings")
+class RoomBookingController(
+    override val endpoint: String = "/api/roombookings",
+
+    override val createPermission: Permission = BACKEND__API_ROOMBOOKINGS__CREATE,
+    override val getPermission: Permission = BACKEND__API_TEMPLATES__GET,
+    override val patchPermission: Permission = BACKEND__API_TEMPLATES__PATCH,
+    override val deletePermission: Permission = BACKEND__API_TEMPLATES__DELETE,
+
+    override val service: RoomBookingService,
+    override val mapper: RoomBookingMapper,
+    override val securityService: SecurityService
+
+) : CRUDCreateController<RoomBookingEntity, CreateRoomBookingRequest, GetRoomBookingResponse>,
+    CRUDGetController<RoomBookingEntity, GetRoomBookingResponse>,
+    CRUDPatchController<RoomBookingEntity, PatchRoomBookingRequest, GetRoomBookingResponse>,
+    CRUDDeleteController<RoomBookingEntity>
--- backend/src/main/kotlin/de/csw/turtle/api/service/RoomBookingService.kt
+++ backend/src/main/kotlin/de/csw/turtle/api/service/RoomBookingService.kt
@@ -1,4 +1,15 @@
 package de.csw.turtle.api.service
 
-class RoomBookingService {
-}
+import de.csw.turtle.api.dto.create.CreateRoomBookingRequest
+import de.csw.turtle.api.dto.get.GetRoomBookingResponse
+import de.csw.turtle.api.dto.patch.PatchRoomBookingRequest
+import de.csw.turtle.api.entity.RoomBookingEntity
+import de.csw.turtle.api.mapper.RoomBookingMapper
+import de.csw.turtle.api.repository.RoomBookingRepository
+import org.springframework.stereotype.Service
+
+@Service
+class RoomBookingService(
+    override val repository: RoomBookingRepository,
+    override val mapper: RoomBookingMapper
+) : CRUDService<RoomBookingEntity, CreateRoomBookingRequest, GetRoomBookingResponse, PatchRoomBookingRequest>()
