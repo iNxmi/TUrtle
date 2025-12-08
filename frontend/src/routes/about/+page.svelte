81659d6e63b576a76e927257bcb9f8e7659b55f3
Jan Rappe <jan.rappe2003@gmail.com>
2025-12-08 23:51:19 +0100

    Added sample about page

--- frontend/src/routes/about/+page.svelte
+++ frontend/src/routes/about/+page.svelte
@@ -1,1 +1,5 @@
-About
+<script>
+    import {PUBLIC_CSW_REPRESENTATIVE_NAME, PUBLIC_CSW_SUPPORT_EMAIL} from '$env/static/public'
+</script>
+<p>Verantwortlicher für die Website: {PUBLIC_CSW_REPRESENTATIVE_NAME}</p>
+<p>Kontakt: {PUBLIC_CSW_SUPPORT_EMAIL}</p>
