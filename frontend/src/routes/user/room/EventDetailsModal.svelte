ccb40470e5cc3739eed9835970c1baec6a3a35cd
Jan Rappe <jan.rappe2003@gmail.com>
2025-12-16 00:59:23 +0100

    Added description and whitelist for calendar in user view

---
+++ frontend/src/routes/user/room/EventDetailsModal.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { Modal, P } from 'flowbite-svelte';
+
+    let {showEventDetailsModal = $bindable(), selectedEvent} = $props();
+
+</script>
+{#if selectedEvent}
+    <Modal title={selectedEvent.title + " "+ (selectedEvent.extendedProps.enableWhitelist ? "(_Invitation only_)" : "")} bind:open={showEventDetailsModal}>
+        <P>{selectedEvent.start.toLocaleDateString()+" "+selectedEvent.start.toLocaleTimeString()+" - "
+        + selectedEvent.end.toLocaleDateString()+" "+selectedEvent.end.toLocaleTimeString()}</P>
+        <P class="font-bold">_Creator_</P>
+        <P>{selectedEvent.extendedProps.creator.id}</P>
+        <P class="font-bold">_Description_</P>
+        <P>{selectedEvent.extendedProps.description}</P>
+    </Modal>
+{/if}
