6e27362dcb6649dd982feb2f593ea2cb72817933
Memphis Herghelegiu <memphispc70@gmail.com>
2025-12-18 08:29:15 +0100

    changed get requests since this is the correct restful solution
    from:
    /api/{resource}/one?id={id}
    
    to:
    /api/{resource}/one/{id}

--- backend/src/main/kotlin/de/csw/turtle/api/controller/GetController.kt
+++ backend/src/main/kotlin/de/csw/turtle/api/controller/GetController.kt
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.Sort.Direction
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestParam
 
 interface GetController<
@@ -21,8 +22,8 @@ interface GetController<
     val permissionService: PermissionService
     val permissionGet: Permission?
 
-    @GetMapping("/one")
-    fun get(@RequestParam id: Long): ResponseEntity<GetResponse> {
+    @GetMapping("/one/{id}")
+    fun get(@PathVariable id: Long): ResponseEntity<GetResponse> {
         if (permissionGet != null)
             permissionService.check(permissionGet!!)
 
