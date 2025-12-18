21e6604b7a669d2c12c185f35d2df31223846787
Memphis Herghelegiu <memphispc70@gmail.com>
2025-12-18 01:43:08 +0100

    improved faq page in frontend

--- frontend/src/routes/faq/+page.svelte
+++ frontend/src/routes/faq/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-    import {Heading, A} from 'flowbite-svelte';
+    import {Heading, A, Accordion, AccordionItem} from 'flowbite-svelte';
     import {m} from '$lib/paraglide/messages.js';
     import Markdown from "svelte-exmarkdown";
 
@@ -12,17 +12,14 @@
         {m.faq__title()}
     </Heading>
 
-    <div class="flex flex-col gap-5">
+    <Accordion flush>
         {#each all as faq, index}
-            <div class="flex flex-col">
-                <Heading tag="h4">
-                    {`${index + 1}. ${faq.title}`}
-                </Heading>
-                <!--TODO make the markdown be styled since tailwindcss removes all styling-->
+            <AccordionItem>
+                {#snippet header()}{`${index + 1}. ${faq.title}`}{/snippet}
                 <Markdown md={faq.content}/>
-            </div>
+            </AccordionItem>
         {/each}
-    </div>
+    </Accordion>
 
-    <A class="text-center" href="/support">{m.faq__problem_not_solved()}</A>
+    <A href="/support">{m.faq__problem_not_solved()}</A>
 </div>
