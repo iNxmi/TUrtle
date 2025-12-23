52ac4272fcd8028d34ace6d608a73d3f290e6850
Jan Rappe <jan.rappe2003@gmail.com>
2025-12-23 17:14:06 +0100

    Updated event API

--- frontend/src/lib/server/events/sampleEvents.js
+++ frontend/src/lib/server/events/sampleEvents.js
@@ -1,224 +1,179 @@
 export const events = [
 	{
+		id: "0",
 		title: 'Team Daily Standup',
-		start: '2025-11-17T11:00:00',
-		end: '2025-11-17T12:00:00',
+		start: '2025-11-17T12:00:00',
+		end: '2025-11-17T13:00:00',
 		extendedProps: {
-			creator: {
-				lastName: "Rappe",
-				firstName: "Jan",
-				id: 1
-			},
+			creator: '1',
 			description: 'Test Description',
 			enableWhitelist: false,
 			whitelist: []
 		}
 	},
 	{
+		id: "1",
 		title: 'Project Alpha Kickoff',
 		start: '2025-11-18T14:30:00',
 		end: '2025-11-18T16:30:00',
 		extendedProps: {
-			creator: {
-				lastName: "Rappe",
-				firstName: "Jan",
-				id: 1
-			},
+			creator: '1',
 			description: 'Test Description',
 			enableWhitelist: false,
 			whitelist: []
 		}
 	},
 	{
+		id: "2",
 		title: 'Client Discovery Call',
 		start: '2025-11-19T09:00:00',
 		end: '2025-11-19T10:30:00',
 		extendedProps: {
-			creator: {
-				lastName: "Rappe",
-				firstName: "Jan",
-				id: 1
-			},
+			creator: '1',
 			description: 'Test Description',
 			enableWhitelist: false,
 			whitelist: []
 		}
 	},
 	{
+		id: "3",
 		title: 'Code Review: Backend Service',
 		start: '2025-11-20T10:00:00',
 		end: '2025-11-20T11:00:00',
 		extendedProps: {
-			creator: {
-				lastName: "Rappe",
-				firstName: "Jan",
-				id: 1
-			},
+			creator: '1',
 			description: 'Test Description',
 			enableWhitelist: false,
 			whitelist: []
 		}
 	},
 	{
+		id: "4",
 		title: 'UI/UX Feedback Meeting',
 		start: '2025-11-21T13:30:00',
 		end: '2025-11-21T15:30:00',
 		extendedProps: {
-			creator: {
-				lastName: "Rappe",
-				firstName: "Jan",
-				id: 1
-			},
+			creator: '1',
 			description: 'Test Description',
 			enableWhitelist: false,
 			whitelist: []
 		}
 	},
 	{
+		id: "5",
 		title: 'Hiring Interview - Candidate 1',
 		start: '2025-11-22T08:00:00',
 		end: '2025-11-22T09:30:00',
 		extendedProps: {
-			creator: {
-				lastName: "Rappe",
-				firstName: "Jan",
-				id: 1
-			},
+			creator: '1',
 			description: 'Test Description',
 			enableWhitelist: false,
 			whitelist: []
 		}
 	},
 	{
+		id: "6",
 		title: 'Q3 Budget Review',
 		start: '2025-11-23T16:00:00',
 		end: '2025-11-23T17:00:00',
 		extendedProps: {
-			creator: {
-				lastName: "Rappe",
-				firstName: "Jan",
-				id: 1
-			},
+			creator: '1',
 			description: 'Test Description',
 			enableWhitelist: false,
 			whitelist: []
 		}
 	},
 	{
+		id: "7",
 		title: 'Infrastructure Check',
 		start: '2025-11-24T10:30:00',
 		end: '2025-11-24T12:30:00',
 		extendedProps: {
-			creator: {
-				lastName: "Rappe",
-				firstName: "Jan",
-				id: 1
-			},
+			creator: '1',
 			description: 'Test Description',
 			enableWhitelist: false,
 			whitelist: []
 		}
 	},
 	{
+		id: "8",
 		title: 'Deep Work Slot (No Interruptions)',
 		start: '2025-11-24T14:00:00',
 		end: '2025-11-24T14:30:00',
 		extendedProps: {
-			creator: {
-				lastName: "Rappe",
-				firstName: "Jan",
-				id: 1
-			},
+			creator: '1',
 			description: 'Test Description',
 			enableWhitelist: false,
 			whitelist: []
 		}
 	},
 	{
+		id: "9",
 		title: 'Sprint Planning Session',
 		start: '2025-11-25T15:30:00',
 		end: '2025-11-25T17:00:00',
 		extendedProps: {
-			creator: {
-				lastName: "Rappe",
-				firstName: "Jan",
-				id: 1
-			},
+			creator: '1',
 			description: 'Test Description',
 			enableWhitelist: false,
 			whitelist: []
 		}
 	},
 	{
+		id: "10",
 		title: '1:1 with Senior Lead',
 		start: '2025-11-26T11:00:00',
 		end: '2025-11-26T12:00:00',
 		extendedProps: {
-			creator: {
-				lastName: "Rappe",
-				firstName: "Jan",
-				id: 1
-			},
+			creator: '1',
 			description: 'Test Description',
 			enableWhitelist: false,
 			whitelist: []
 		}
 	},
 	{
+		id: "11",
 		title: 'Documentation Workshop',
 		start: '2025-11-27T14:30:00',
 		end: '2025-11-27T16:30:00',
 		extendedProps: {
-			creator: {
-				lastName: "Rappe",
-				firstName: "Jan",
-				id: 1
-			},
+			creator: '1',
 			description: 'Test Description',
 			enableWhitelist: false,
 			whitelist: []
 		}
 	},
 	{
+		id: "12",
 		title: 'Investor Update Prep',
 		start: '2025-11-28T09:30:00',
 		end: '2025-11-28T10:30:00',
 		extendedProps: {
-			creator: {
-				lastName: "Rappe",
-				firstName: "Jan",
-				id: 1
-			},
+			creator: "1",
 			description: 'Test Description',
 			enableWhitelist: false,
 			whitelist: []
 		}
 	},
 	{
+		id: "13",
 		title: 'Marketing Strategy Sync',
 		start: '2025-11-29T10:00:00',
 		end: '2025-11-29T10:30:00',
 		extendedProps: {
-			creator: {
-				lastName: "Rappe",
-				firstName: "Jan",
-				id: 1
-			},
+			creator: "1",
 			description: 'Test Description',
 			enableWhitelist: false,
 			whitelist: []
 		}
 	},
 	{
+		id: "14",
 		title: 'QA Testing Huddle',
 		start: '2025-11-30T16:30:00',
 		end: '2025-11-30T17:30:00',
 		extendedProps: {
-			creator: {
-				lastName: "Rappe",
-				firstName: "Jan",
-				id: 1
-			},
+			creator: "1",
 			description: 'Test Description',
 			enableWhitelist: true,
 			whitelist: []
