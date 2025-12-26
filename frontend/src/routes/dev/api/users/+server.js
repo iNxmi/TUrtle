7b4d192823b1ce68d206d04ead48d666962814c0
Jan Rappe <jan.rappe2003@gmail.com>
2025-12-26 15:48:04 +0100

    Fixed dev mode api

--- frontend/src/routes/dev/api/users/+server.js
+++ frontend/src/routes/dev/api/users/+server.js
@@ -1,6 +1,6 @@
 import { userDatabase } from '$lib/server/DatabaseInitializer';
 import { json } from '@sveltejs/kit';
 
-export async function GET() {
-	return json(userDatabase.getUsers());
+export async function GET({url}) {
+	return json(url.searchParams.get('pageNumber') ? {content: userDatabase.getUsers(), page:{size: 10, number: 0, totalElements:10, totalPages:1} }:userDatabase.getUsers());
 }
