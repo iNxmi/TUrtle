ef6086f6876eaee475dc0c162e0307d32c3016e1
Jan Rappe <jan.rappe2003@gmail.com>
2026-01-01 21:24:12 +0100

    Added authorization check

--- frontend/src/routes/+layout.js
+++ frontend/src/routes/+layout.js
@@ -1,10 +1,12 @@
 import request from "$lib/api/api.js";
+import { checkAuthorization } from "$lib/utils";
 
 export const prerender = false;
 export const ssr = false;
 
 export async function load() {
     const permissionsResponse = await request("/permissions");
+    checkAuthorization(permissionsResponse);
     let permissions = {}
     if (permissionsResponse.status === 200)
         permissions = await permissionsResponse.json();
