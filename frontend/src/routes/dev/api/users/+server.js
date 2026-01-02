9f9ed12791ed8b029f9a6233590edcfc77aef00b
Jan Rappe <jan.rappe2003@gmail.com>
2026-01-02 15:39:37 +0100

    Fixed devmode api

--- frontend/src/routes/dev/api/users/+server.js
+++ frontend/src/routes/dev/api/users/+server.js
@@ -4,3 +4,8 @@ import { json } from '@sveltejs/kit';
 export async function GET({url}) {
 	return json(url.searchParams.get('pageNumber') ? {content: userDatabase.getUsers(), page:{size: 10, number: 0, totalElements:10, totalPages:1} }:userDatabase.getUsers());
 }
+export async function POST({request}){
+	const userData = await request.json();
+	userDatabase.createUser(userData);
+	return json({userData}, {status:201});
+}
