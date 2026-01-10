2e7f34c3a2f622bcf34060c2b72be3e3bedc951d
Jan Rappe <jan.rappe2003@gmail.com>
2026-01-10 21:24:47 +0000

    Added device bookings to devmode api

---
+++ frontend/src/routes/dev/api/devicebookings/+server.js
@@ -0,0 +1,30 @@
+import { json } from '@sveltejs/kit';
+
+export async function GET(){
+    return json([
+        {
+            deviceName: "Dell Laptop",
+            start: "2026-01-05",
+            end: "2026-01-07",
+            locker: "5"
+        },
+         {
+            deviceName: "Asus Laptop",
+            start: "2026-01-06",
+            end: "2026-01-08",
+            locker: "4"
+        },
+         {
+            deviceName: "IPad",
+            start: "2026-01-02",
+            end: "2026-01-04",
+            locker: "4"
+        },
+         {
+            deviceName: "Beamer",
+            start: "2026-01-03",
+            end: "2026-01-05",
+            locker: "7"
+        }
+    ]);
+}
--- frontend/src/routes/dev/api/roombookings/+server.js
+++ frontend/src/routes/dev/api/roombookings/+server.js
@@ -2,18 +2,5 @@ import { eventDatabase } from '$lib/server/DatabaseInitializer';
 import { json } from '@sveltejs/kit';
 
 export async function GET() {
-	return json(eventDatabase.getEvents());
-}
-
-export async function DELETE({ request }) {
-	const id = await request.json();
-	eventDatabase.deleteEventById(id.toString());
-
-	return new Response(null, { status: 204 });
-}
-
-export async function POST({ request }) {
-	const event = await request.json();
-	eventDatabase.createEvent(event);
-	return new Response(null, { status: 200 });
+    return json(eventDatabase.getEvents());
 }
