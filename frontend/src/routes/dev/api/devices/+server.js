0869d0e23c90897ec498e6e33effe3313cf4b7a0
Jan Rappe <jan.rappe2003@gmail.com>
2026-01-13 11:52:16 +0100

    Changesdevmode api

--- frontend/src/routes/dev/api/devices/+server.js
+++ frontend/src/routes/dev/api/devices/+server.js
@@ -2,15 +2,15 @@ import {json} from '@sveltejs/kit';
 export async function GET(){
 
     const devices  = [{  
-        name:"iPad", category: "1", id: "0"
+        name:"iPad", category: "1", id: "0", locker: "3"
     },{  
-        name:"Dell Laptop", category: "0", id: "1"
+        name:"Dell Laptop", category: "0", id: "1", locker: "5"
     },{  
-        name:"Asus Laptop", category: "0", id: "2"
+        name:"Asus Laptop", category: "0", id: "2", locker: "6"
     },{  
-        name:"Samsung Tab A", category: "1", id: "3"
+        name:"Samsung Tab A", category: "1", id: "3", locker: "4"
     },{  
-        name:"Acer Laptop", category: "0", id: "4"
+        name:"Acer Laptop", category: "0", id: "4", locker: "5"
     },
     ];
 
