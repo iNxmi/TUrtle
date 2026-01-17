ffa44fc0bce5d7f4e6b0211d247d7611d8a33980
Jan Rappe <jan.rappe2003@gmail.com>
2026-01-17 17:34:36 +0100

    Improved devmode api

--- frontend/src/routes/dev/api/devicebookings/+server.js
+++ frontend/src/routes/dev/api/devicebookings/+server.js
@@ -3,27 +3,35 @@ import { json } from '@sveltejs/kit';
 export async function GET(){
     return json([
         {
-            deviceName: "Dell Laptop",
+            id: "0",
+            name: "Dell Laptop",
             start: "2026-01-05T06:00Z",
             end: "2026-01-07T06:00Z",
+            deviceId: "0",
             locker: "5"
         },
          {
-            deviceName: "Asus Laptop",
+            id: "1",
+            name: "Asus Laptop",
             start: "2026-01-06T06:00Z",
             end: "2026-01-08T06:00Z",
+            deviceId: "1",
             locker: "4"
         },
          {
-            deviceName: "IPad",
+            id: "2",
+            name: "IPad",
             start: "2026-01-02T06:00Z",
             end: "2026-01-04T06:00Z",
+            deviceId: "2",
             locker: "4"
         },
          {
-            deviceName: "Beamer",
+            id: "3",
+            name: "Beamer",
             start: "2026-01-03T06:00Z",
             end: "2026-01-05T06:00Z",
+            deviceId: "3",
             locker: "7"
         }
     ]);
