c0484c7c56228a36f1d58141a4c4524892df1cfc
Jan Rappe <jan.rappe2003@gmail.com>
2026-01-17 17:34:17 +0100

    Added open locker modal

---
+++ frontend/src/lib/components/OpenLockerModal.svelte
@@ -0,0 +1,17 @@
+<script>
+    import {Modal, Button} from 'flowbite-svelte';
+    import {openLocker} from '$lib/utils'
+    import {slide} from 'svelte/transition'
+    let { showOpenLockerModal = $bindable(false), locker} = $props();
+
+
+</script>
+<Modal transition={slide} bind:open={showOpenLockerModal} size="xs" permanent>
+    <div class="text-center">
+       <h3 class="mb-5 text-lg"> _Do you want to open locker <span class="text-csw font-bold">{locker}</span> now?_</h3>
+        <div class="space-x-2">
+            <Button class="hover:cursor-pointer" onclick={() => {openLocker(locker); showOpenLockerModal=false}}>_Yes_</Button>
+            <Button class="hover:cursor-pointer" color="red" onclick={() => showOpenLockerModal = false}>_No_</Button>
+        </div>
+    </div>
+</Modal>
