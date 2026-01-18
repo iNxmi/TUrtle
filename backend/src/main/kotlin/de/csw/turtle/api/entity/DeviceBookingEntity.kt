3ef5c63e8c6978e5155dcaf85cadc80c752e4006
Memphis Herghelegiu <memphispc70@gmail.com>
2026-01-18 15:34:17 +0100

    implemented status for device bookings (#81 on github)

--- backend/src/main/kotlin/de/csw/turtle/api/entity/DeviceBookingEntity.kt
+++ backend/src/main/kotlin/de/csw/turtle/api/entity/DeviceBookingEntity.kt
@@ -2,6 +2,8 @@ package de.csw.turtle.api.entity
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
