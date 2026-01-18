0fbe04ee0eaa65f433abd56665457556b06534ec
Jan Rappe <jan.rappe2003@gmail.com>
2026-01-18 20:26:53 +0100

    Implemented collection and return process

--- frontend/src/lib/components/DevicebookingTable.svelte
+++ frontend/src/lib/components/DevicebookingTable.svelte
@@ -6,6 +6,7 @@
     let {reservations, devices } = $props();
     let showOpenLockerModal = $state(false);
     let locker = $state();
+    let selectedReservationIndex = $state();
 
     let bookingInfo = $derived(reservations.map((reservation) => {
         const device = devices.find((device) =>  reservation.deviceId === device.id);
@@ -14,32 +15,31 @@
             start: new Date(reservation.start), 
             end: new Date(reservation.end),
             locker: device.locker,
-            id: reservation.id
+            id: reservation.id,
+            status: reservation.status
         }
     }));
     let dt = $derived(Intl.DateTimeFormat(getContext('locale'), {dateStyle: "medium",timeStyle: "medium"}));
 
-    $effect(() => {
+    function handleReservationInteraction(reservationIndex){
 
-        setTimeout(() => {
-            document.getElementById("0").classList.add("border-orange-400!");
-            document.getElementById("0").classList.add("border-3");
-        }, 1000);
-    });
+        selectedReservationIndex = reservationIndex;
+        locker = bookingInfo[reservationIndex].locker;
+        showOpenLockerModal = true;
+
+    }
 </script>
 <div class="flex flex-col gap-4 max-h-svh overflow-y-auto">
-    {#each bookingInfo as reservation, i}
-        {#if i === 0}
-         <button id={reservation.id} onclick={() => {
-            locker = reservation.locker;
-            showOpenLockerModal = true}} class="hover:cursor-pointer h-20 bg-gray-50 dark:bg-gray-700 border rounded-lg border-gray-100 dark:border-gray-800 shadow grid grid-flow-row grid-rows-1 grid-cols-4">
+    {#each bookingInfo as reservation, i (reservation.id)}
+        {#if reservation.status === "COLLECTION_READY" || reservation.status === "RESERVATION_ENDED"}
+         <button onclick={() => handleReservationInteraction(i)} class="hover:cursor-pointer h-20 bg-gray-50 dark:bg-gray-700 border-3 border-orange-400! rounded-lg dark:border-gray-800 shadow grid grid-flow-row grid-rows-1 grid-cols-4">
             <div class="place-self-center"><div class="flex flex-col text-center"><span class="text-sm text-muted">_Device_</span><span class="font-bold text-lg dark:text-white">{reservation.deviceName}</span></div></div>
             <div class="place-self-center"><div class="flex flex-col text-center"><span class="text-sm text-muted">_Start date_</span><span class="font-bold text-lg dark:text-white">{dt.format(reservation.start)}</span></div></div>
             <div class="place-self-center"><div class="flex flex-col text-center"><span class="text-sm text-muted">_EndDate_</span><span class="font-bold text-lg dark:text-white">{dt.format(reservation.end)}</span></div></div>
             <div class="place-self-center"><div class="flex flex-col text-center"><span class="text-sm text-muted">_Locker_</span><span class="font-bold text-lg dark:text-white">{reservation.locker}</span></div></div> 
         </button>
         {:else}
-        <div id={reservation.id} class="h-20 bg-gray-50 dark:bg-gray-700 border rounded-lg border-gray-100 dark:border-gray-800 shadow grid grid-flow-row grid-rows-1 grid-cols-4">
+        <div class="h-20 bg-gray-50 dark:bg-gray-700 border rounded-lg border-gray-100 dark:border-gray-800 shadow grid grid-flow-row grid-rows-1 grid-cols-4">
             <div class="place-self-center"><div class="flex flex-col text-center"><span class="text-sm text-muted">_Device_</span><span class="font-bold text-lg dark:text-white">{reservation.deviceName}</span></div></div>
             <div class="place-self-center"><div class="flex flex-col text-center"><span class="text-sm text-muted">_Start date_</span><span class="font-bold text-lg dark:text-white">{dt.format(reservation.start)}</span></div></div>
             <div class="place-self-center"><div class="flex flex-col text-center"><span class="text-sm text-muted">_EndDate_</span><span class="font-bold text-lg dark:text-white">{dt.format(reservation.end)}</span></div></div>
@@ -50,4 +50,4 @@
         <div class="h-100 flex justify-center items-center"><span class="text-center font-bold text-3xl text-muted">_No reservations made_</span></div>
     {/each}
 </div>
-<OpenLockerModal bind:showOpenLockerModal={showOpenLockerModal} {locker}></OpenLockerModal>
+<OpenLockerModal bind:showOpenLockerModal={showOpenLockerModal} {locker} reservationIndex={selectedReservationIndex} bind:reservations={reservations}/>
