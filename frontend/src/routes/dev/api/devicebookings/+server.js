248c445334c3e2fe16dcb64d01448144c028872b
Jan Rappe <jan.rappe2003@gmail.com>
2026-01-18 20:22:53 +0100

    Updated devmode

--- frontend/src/routes/dev/api/devicebookings/+server.js
+++ frontend/src/routes/dev/api/devicebookings/+server.js
@@ -8,7 +8,8 @@ export async function GET(){
             start: "2026-01-05T06:00Z",
             end: "2026-01-07T06:00Z",
             deviceId: "0",
-            locker: "5"
+            locker: "5",
+            status: "COLLECTION_READY"
         },
          {
             id: "1",
@@ -16,7 +17,8 @@ export async function GET(){
             start: "2026-01-06T06:00Z",
             end: "2026-01-08T06:00Z",
             deviceId: "1",
-            locker: "4"
+            locker: "4",
+            status: "RESERVATION_ENDED"
         },
          {
             id: "2",
@@ -24,7 +26,8 @@ export async function GET(){
             start: "2026-01-02T06:00Z",
             end: "2026-01-04T06:00Z",
             deviceId: "2",
-            locker: "4"
+            locker: "4",
+            status: "DEVICE_RETURNED"
         },
          {
             id: "3",
@@ -32,7 +35,11 @@ export async function GET(){
             start: "2026-01-03T06:00Z",
             end: "2026-01-05T06:00Z",
             deviceId: "3",
-            locker: "7"
+            locker: "7",
+            status: "DEVICE_RETURNED"
         }
     ]);
+}
+export async function PATCH(){
+    return json(true, {status:200});
 }
