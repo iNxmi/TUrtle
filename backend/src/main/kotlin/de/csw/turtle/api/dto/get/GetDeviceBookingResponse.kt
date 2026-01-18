ea881ff4b4a5cc5506d809a869847d9e5ea77228
Memphis Herghelegiu <memphispc70@gmail.com>
2026-01-18 21:19:02 +0100

    fixed null pointer exception on get for any entity, it should now just represent the state in the database

--- backend/src/main/kotlin/de/csw/turtle/api/dto/get/GetDeviceBookingResponse.kt
+++ backend/src/main/kotlin/de/csw/turtle/api/dto/get/GetDeviceBookingResponse.kt
@@ -4,11 +4,11 @@ import de.csw.turtle.api.entity.DeviceBookingEntity
 import java.time.Instant
 
 class GetDeviceBookingResponse(
-    override val id: Long,
-    val start: Instant,
-    val end: Instant,
-    val deviceId: Long,
-    val userId: Long,
-    val status : DeviceBookingEntity.Status,
-    override val createdAt: Instant
+    override val id: Long?,
+    val start: Instant?,
+    val end: Instant?,
+    val deviceId: Long?,
+    val userId: Long?,
+    val status : DeviceBookingEntity.Status?,
+    override val createdAt: Instant?
 ) : GetResponse
