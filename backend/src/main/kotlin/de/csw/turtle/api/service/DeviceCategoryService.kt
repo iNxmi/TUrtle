a9ad266b2bbddd0226404e81d1fef856dd725c31
Z9B <t.thanh.lam.nguyen@gmail.com>
2026-01-27 01:26:13 +0100

    update register page link and add conflict checks in DeviceCategoryService

--- backend/src/main/kotlin/de/csw/turtle/api/service/DeviceCategoryService.kt
+++ backend/src/main/kotlin/de/csw/turtle/api/service/DeviceCategoryService.kt
@@ -4,6 +4,7 @@ import de.csw.turtle.api.dto.create.CreateDeviceCategoryRequest
 import de.csw.turtle.api.dto.get.GetDeviceCategoryResponse
 import de.csw.turtle.api.dto.patch.PatchDeviceCategoryRequest
 import de.csw.turtle.api.entity.DeviceCategoryEntity
+import de.csw.turtle.api.exception.ConflictException
 import de.csw.turtle.api.exception.NotFoundException
 import de.csw.turtle.api.mapper.DeviceCategoryMapper
 import de.csw.turtle.api.repository.DeviceCategoryRepository
@@ -16,7 +17,22 @@ class DeviceCategoryService(
 ) : CRUDService<DeviceCategoryEntity, CreateDeviceCategoryRequest, GetDeviceCategoryResponse, PatchDeviceCategoryRequest>(
     "DeviceCategory"
 ) {
-
+    fun getByNameOrNull(name: String): DeviceCategoryEntity? = repository.findByName(name)
     fun getByName(name: String) = repository.findByName(name) ?: throw NotFoundException(name)
+
+    override fun create(request: CreateDeviceCategoryRequest): DeviceCategoryEntity {
+        if(getByNameOrNull(request.name) != null)
+            throw ConflictException("DeviceCategory with name '${request.name}' already exists.")
+
+        return super.create(request)
+    }
+
+    override fun patch(id: Long, request: PatchDeviceCategoryRequest): DeviceCategoryEntity {
+        if(request.name != null)
+            if(getByNameOrNull(request.name) != null)
+                throw ConflictException("DeviceCategory with name '${request.name}' already exists.")
+
+        return super.patch(id, request)
+    }
 
 }
