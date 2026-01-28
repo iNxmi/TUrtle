770af6ba98740cb4b035217927804966e9bd3802
Memphis Herghelegiu <memphispc70@gmail.com>
2026-01-28 17:44:14 +0100

    reimplemented audit log controller

--- backend/src/main/kotlin/de/csw/turtle/api/controller/api/AuditLogController.kt
+++ backend/src/main/kotlin/de/csw/turtle/api/controller/api/AuditLogController.kt
@@ -1,14 +1,16 @@
 package de.csw.turtle.api.controller.api
 
 import de.csw.turtle.api.Permission
-import de.csw.turtle.api.entity.UserEntity
 import de.csw.turtle.api.controller.GetController
 import de.csw.turtle.api.dto.get.GetAuditLogResponse
 import de.csw.turtle.api.entity.AuditLogEntity
+import de.csw.turtle.api.entity.UserEntity
 import de.csw.turtle.api.exception.ForbiddenException
 import de.csw.turtle.api.exception.UnauthorizedException
 import de.csw.turtle.api.mapper.AuditLogMapper
 import de.csw.turtle.api.service.AuditLogService
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Sort
 import org.springframework.data.domain.Sort.Direction
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RequestMapping
@@ -19,8 +21,7 @@ import org.springframework.web.bind.annotation.RestController
 class AuditLogController(
     private val auditLogService: AuditLogService,
     private val auditLogMapper: AuditLogMapper
-) :
-    GetController<AuditLogEntity, GetAuditLogResponse> {
+) : GetController<AuditLogEntity, GetAuditLogResponse> {
 
     override fun get(
         user: UserEntity?,
@@ -29,10 +30,11 @@ class AuditLogController(
         if (user == null)
             throw UnauthorizedException()
 
-        if (!user.hasPermission(Permission.MANAGE_AUDIT_LOGS))
-            throw ForbiddenException()
-
         val entity = auditLogService.get(id)
+        if (!user.hasPermission(Permission.MANAGE_AUDIT_LOGS))
+            if (entity.user != user)
+                throw ForbiddenException()
+
         val dto = auditLogMapper.get(entity)
         return ResponseEntity.ok(dto)
     }
@@ -45,7 +47,34 @@ class AuditLogController(
         sortProperty: String?,
         sortDirection: Direction
     ): ResponseEntity<Any> {
-        TODO("Not yet implemented")
+        if (user == null)
+            throw UnauthorizedException()
+
+        if (!user.hasPermission(Permission.MANAGE_AUDIT_LOGS))
+            throw ForbiddenException()
+
+        val sort = sortProperty?.let {
+            Sort.by(sortDirection, sortProperty)
+        } ?: Sort.unsorted()
+
+        if (pageNumber != null) {
+            val pageable = PageRequest.of(pageNumber, pageSize, sort)
+            val page = auditLogService.getPage(rsql = rsql, pageable = pageable)
+
+            if (!user.hasPermission(Permission.MANAGE_DEVICE_BOOKINGS))
+                page.removeAll { it.user != user }
+
+            val dto = page.map { auditLogMapper.get(it) }
+            return ResponseEntity.ok(dto)
+        }
+
+        val collection = auditLogService.getAll(rsql = rsql, sort = sort).toMutableSet()
+
+        if (!user.hasPermission(Permission.MANAGE_DEVICE_BOOKINGS))
+            collection.removeAll { it.user != user }
+
+        val dto = collection.map { auditLogMapper.get(it) }
+        return ResponseEntity.ok(dto)
     }
 
 }
