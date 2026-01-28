52b44a58fcb2cff320ee230bb12f8570e855a275
Memphis Herghelegiu <memphispc70@gmail.com>
2026-01-28 19:16:59 +0100

    reimplemented system setting controller

--- backend/src/main/kotlin/de/csw/turtle/api/controller/api/SystemSettingController.kt
+++ backend/src/main/kotlin/de/csw/turtle/api/controller/api/SystemSettingController.kt
@@ -1,11 +1,17 @@
 package de.csw.turtle.api.controller.api
 
-import de.csw.turtle.api.entity.UserEntity
+import de.csw.turtle.api.Permission
 import de.csw.turtle.api.controller.GetController
 import de.csw.turtle.api.controller.PatchController
 import de.csw.turtle.api.dto.get.GetSystemSettingResponse
 import de.csw.turtle.api.dto.patch.PatchSystemSettingRequest
 import de.csw.turtle.api.entity.SystemSettingEntity
+import de.csw.turtle.api.entity.UserEntity
+import de.csw.turtle.api.exception.ForbiddenException
+import de.csw.turtle.api.exception.UnauthorizedException
+import de.csw.turtle.api.mapper.SystemSettingMapper
+import de.csw.turtle.api.service.SystemSettingService
+import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Sort
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RequestMapping
@@ -13,15 +19,26 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/system-settings")
-class SystemSettingController : 
-    GetController<SystemSettingEntity, GetSystemSettingResponse>,
+class SystemSettingController(
+    private val systemSettingService: SystemSettingService,
+    private val systemSettingMapper: SystemSettingMapper
+) : GetController<SystemSettingEntity, GetSystemSettingResponse>,
     PatchController<SystemSettingEntity, PatchSystemSettingRequest, GetSystemSettingResponse> {
+
 
     override fun get(
         user: UserEntity?,
         id: Long
     ): ResponseEntity<GetSystemSettingResponse> {
-        TODO("Not yet implemented")
+        if (user == null)
+            throw UnauthorizedException()
+
+        if (!user.hasPermission(Permission.MANAGE_SYSTEM_SETTINGS))
+            throw ForbiddenException()
+
+        val entity = systemSettingService.get(id)
+        val dto = systemSettingMapper.get(entity)
+        return ResponseEntity.ok(dto)
     }
 
     override fun getCollection(
@@ -32,7 +49,26 @@ class SystemSettingController :
         sortProperty: String?,
         sortDirection: Sort.Direction
     ): ResponseEntity<Any> {
-        TODO("Not yet implemented")
+        if (user == null)
+            throw UnauthorizedException()
+
+        if (!user.hasPermission(Permission.MANAGE_SYSTEM_SETTINGS))
+            throw ForbiddenException()
+
+        val sort = sortProperty?.let {
+            Sort.by(sortDirection, sortProperty)
+        } ?: Sort.unsorted()
+
+        if (pageNumber != null) {
+            val pageable = PageRequest.of(pageNumber, pageSize, sort)
+            val page = systemSettingService.getPage(rsql = rsql, pageable = pageable)
+            val dto = page.map { systemSettingMapper.get(it) }
+            return ResponseEntity.ok(dto)
+        }
+
+        val collection = systemSettingService.getAll(rsql = rsql, sort = sort).toMutableSet()
+        val dto = collection.map { systemSettingMapper.get(it) }
+        return ResponseEntity.ok(dto)
     }
 
     override fun patch(
@@ -40,7 +76,15 @@ class SystemSettingController :
         id: Long,
         request: PatchSystemSettingRequest
     ): ResponseEntity<GetSystemSettingResponse> {
-        TODO("Not yet implemented")
+        if (user == null)
+            throw UnauthorizedException()
+
+        if (!user.hasPermission(Permission.MANAGE_SYSTEM_SETTINGS))
+            throw ForbiddenException()
+
+        val updated = systemSettingService.patch(id, request)
+        val dto = systemSettingMapper.get(updated)
+        return ResponseEntity.ok(dto)
     }
 
 }
