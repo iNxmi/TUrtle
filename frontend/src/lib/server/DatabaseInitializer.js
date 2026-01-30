a895b23c13cce8f31904775d63432b9ba6d6fee5
Jan Rappe <jan.rappe2003@gmail.com>
2026-01-30 14:21:56 +0100

    Extended frontend database

--- frontend/src/lib/server/DatabaseInitializer.js
+++ frontend/src/lib/server/DatabaseInitializer.js
@@ -2,9 +2,13 @@ import SimpleDB from './SimpleDB';
 import { UserDatabase } from './users/UserDatabase';
 import { EventDatabase } from './events/EventDatabase';
 import { seedEvents } from './events/sampleEvents';
+import { DevicebookingDatabase } from './devicebookings/DevicebookingDatabase';
+import { seedDevicebookings } from './devicebookings/SampleDevicebookings';
 const sharedDB = new SimpleDB();
 
 export const userDatabase = new UserDatabase(sharedDB);
 userDatabase.seedDatabase();
 export const eventDatabase = new EventDatabase(sharedDB);
 seedEvents(eventDatabase);
+export const devicebookingDatabase = new DevicebookingDatabase(sharedDB);
+seedDevicebookings(devicebookingDatabase);
