3aff4de19634f1ecd26404721b36594d0f6f3246
Jan Rappe <jan.rappe2003@gmail.com>
2026-01-30 14:21:56 +0100

    Extended frontend database

---
+++ frontend/src/lib/server/devicebookings/DevicebookingDatabase.js
@@ -0,0 +1,28 @@
+export class DevicebookingDatabase{
+    constructor(db) {
+        this.db = db;
+        this.collection = 'devicebookings';
+        this.id = 0;
+    }
+
+    createDevicebooking(booking){
+        this.db.create(this.collection, {
+            id: this.id.toString(),
+            ...booking
+        });
+        this.id += 1;
+    }
+    getDevicebookings(){
+        return this.db.read(this.collection);
+    }
+    getDevicebookingById(id){
+        return this.db.read(this.collection, id);
+    }
+    deleteDevicebookingById(id){
+        this.db.delete(this.collection, id);
+    }
+    updateDevicebookingById(id, booking){
+        this.db.update(this.collection, id, booking);
+    }
+
+}
---
+++ frontend/src/lib/server/devicebookings/SampleDevicebookings.js
@@ -0,0 +1,47 @@
+export const seedDevicebookings = (db) => {
+
+    const bookings = [
+        {
+            name: "Dell Laptop",
+            start: "2026-01-05T06:00Z",
+            end: "2026-01-07T06:00Z",
+            deviceId: "0",
+            userId: "0",
+            locker: "5",
+            status: "COLLECTION_READY",
+            createdAt: new Date(Date.now())
+        },
+         {
+            name: "Asus Laptop",
+            start: "2026-01-06T06:00Z",
+            end: "2026-01-08T06:00Z",
+            deviceId: "1",
+            userId: "1",
+            locker: "4",
+            status: "RESERVATION_ENDED",
+            createdAt: new Date(Date.now())
+        },
+         {
+            name: "IPad",
+            start: "2026-01-02T06:00Z",
+            end: "2026-01-04T06:00Z",
+            deviceId: "2",
+            userId: "2",
+            locker: "4",
+            status: "DEVICE_RETURNED",
+            createdAt: new Date(Date.now())
+        },
+         {
+            name: "Beamer",
+            start: "2026-01-03T06:00Z",
+            end: "2026-01-05T06:00Z",
+            deviceId: "3",
+            userId: "3",
+            locker: "7",
+            status: "DEVICE_RETURNED",
+            createdAt: new Date(Date.now())
+        }
+    ];
+
+    bookings.forEach((booking) => db.createDevicebooking(booking));
+}
