51518c8008dfba6cc5f286def998dfff122900ce
Jan Rappe <jan.rappe2003@gmail.com>
2026-02-02 00:24:37 +0100

    Implemented workaround for permission changes

--- frontend/src/routes/+layout.js
+++ frontend/src/routes/+layout.js
@@ -1,5 +1,5 @@
 import request from "$lib/api/api.js";
-import { checkAuthorization } from "$lib/utils";
+/* import { checkAuthorization } from "$lib/utils"; */
 import { permissionsPath, usersPath} from '$lib/backend'
 /* import { jwt } from './state.svelte' */
 
@@ -10,15 +10,35 @@ export async function load() {
 
     const permissionsResponse = await request(permissionsPath);
 
-   await checkAuthorization(permissionsResponse);
+     const itemsPublic = [
+        "VIEW__SIDEBAR_CATEGORY__PUBLIC",
+
+        "VIEW__SIDEBAR_ITEM__HOME",
+    
+        "VIEW__SIDEBAR_ITEM__LOGIN",
+    
+        "VIEW__SIDEBAR_ITEM__REGISTER",
+
+        "VIEW__SIDEBAR_ITEM__SUPPORT",
+
+        "VIEW__SIDEBAR_ITEM__FAQ",
+
+        "VIEW__SIDEBAR_ITEM__ABOUT"
+     ];
+  /*  await checkAuthorization(permissionsResponse); */
     let permissions = {}
-    if (permissionsResponse.status === 200)
+    if (permissionsResponse.status === 200){
         permissions = await permissionsResponse.json();
+    } else {
+        permissions = itemsPublic;
+    }
+        
 
     const profileResponse = await request("/user/profile");
     let user = {}
     if (profileResponse.status === 200)
         user = await profileResponse.json();
 
+    console.log(JSON.stringify(permissions));
     return {permissions, user};
 }
