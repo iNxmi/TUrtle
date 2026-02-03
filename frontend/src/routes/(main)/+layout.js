ebd718df704e6dfd2a4397ccafb0430d333e2955
Jan Rappe <jan.rappe2003@gmail.com>
2026-02-03 23:01:38 +0100

    Fixed infinite recursion in not logged in

--- frontend/src/routes/(main)/+layout.js
+++ frontend/src/routes/(main)/+layout.js
@@ -5,19 +5,21 @@ import { permissionsPath, authPath} from '$lib/backend'
 export const prerender = false;
 export const ssr = false;
 
-export async function load() {
+export async function load({url}) {
 
+    let user = {} 
+    let userPermissions = []
+    if(url.pathname === '/auth/login'){
+        return {userPermissions, user};
+    }
     const permissionsResponse = await request(permissionsPath);
 
-  /*  await checkAuthorization(permissionsResponse); */
-    let userPermissions = []
+   
     if (permissionsResponse.status === 200){
         userPermissions = await permissionsResponse.json();
-    }
-        
-
+    }   
     const profileResponse = await request(authPath+'/me');
-    let user = {}
+
     if (profileResponse.status === 200)
         user = await profileResponse.json();
 
