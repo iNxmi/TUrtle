d444a1d7479b61eab54d29488dcb4f1a4da60d93
Memphis Herghelegiu <memphispc70@gmail.com>
2026-02-06 08:08:23 +0100

    removed all mappers

--- backend/src/main/kotlin/de/csw/turtle/api/service/locker/LockerService.kt
+++ backend/src/main/kotlin/de/csw/turtle/api/service/locker/LockerService.kt
@@ -1,19 +1,14 @@
 package de.csw.turtle.api.service.locker
 
-import de.csw.turtle.api.dto.create.CreateLockerRequest
-import de.csw.turtle.api.dto.get.GetLockerResponse
-import de.csw.turtle.api.dto.patch.PatchLockerRequest
 import de.csw.turtle.api.entity.LockerEntity
-import de.csw.turtle.api.mapper.LockerMapper
 import de.csw.turtle.api.repository.LockerRepository
 import de.csw.turtle.api.service.CRUDService
 import org.springframework.stereotype.Service
 
 @Service
 class LockerService(
-    override val repository: LockerRepository,
-    override val mapper: LockerMapper,
-) : CRUDService<LockerEntity, CreateLockerRequest, GetLockerResponse, PatchLockerRequest>("Locker") {
+    override val repository: LockerRepository
+) : CRUDService<LockerEntity>("Locker") {
 
     fun getByIndex(index: Int) = repository.findByIndex(index)
 
