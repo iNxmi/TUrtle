c98ff5bb6d005eb508139af506ce2b3a78f346b2
Jan Rappe <jan.rappe2003@gmail.com>
2026-02-06 13:19:23 +0100

    grouped user paths into user

--- frontend/src/routes/(main)/contact/+page.js
+++
@@ -1,9 +0,0 @@
-import request from "$lib/api/api.js";
-import {contentPath} from '$lib/backend'
-
-export async function load() {
-    const response = await request(contentPath + `/contact`);
-    const payload = await response.text();
-
-    return {content: payload};
-}
