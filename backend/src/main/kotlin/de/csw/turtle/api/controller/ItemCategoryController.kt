ebc170f30a1c7abd49ebacdff3ff500539b4ed27
Memphis Herghelegiu <memphispc70@gmail.com>
2026-02-11 23:22:20 +0100

    fixed /api/item-categories

--- backend/src/main/kotlin/de/csw/turtle/api/controller/ItemCategoryController.kt
+++ backend/src/main/kotlin/de/csw/turtle/api/controller/ItemCategoryController.kt
@@ -14,7 +14,14 @@ import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Sort
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.net.URI
 
@@ -29,10 +36,11 @@ class ItemCategoryController(
     PatchController<ItemCategoryEntity, PatchItemCategoryRequest, GetItemCategoryResponse>,
     DeleteController<ItemCategoryEntity> {
 
+    @PostMapping
     override fun create(
         @AuthenticationPrincipal user: UserEntity?,
 
-        request: CreateItemCategoryRequest,
+        @RequestBody request: CreateItemCategoryRequest,
 
         httpRequest: HttpServletRequest,
         httpResponse: HttpServletResponse
@@ -55,10 +63,11 @@ class ItemCategoryController(
         return ResponseEntity.created(location).body(dto)
     }
 
+    @GetMapping("/{variable}")
     override fun get(
         @AuthenticationPrincipal user: UserEntity?,
 
-        variable: Long,
+        @PathVariable variable: Long,
 
         httpRequest: HttpServletRequest,
         httpResponse: HttpServletResponse
@@ -70,14 +79,15 @@ class ItemCategoryController(
         return ResponseEntity.ok(dto)
     }
 
+    @GetMapping
     override fun getCollection(
         @AuthenticationPrincipal user: UserEntity?,
 
-        rsql: String?,
-        pageNumber: Int?,
-        pageSize: Int,
-        sortProperty: String?,
-        sortDirection: Sort.Direction,
+        @RequestParam rsql: String?,
+        @RequestParam pageNumber: Int?,
+        @RequestParam pageSize: Int,
+        @RequestParam sortProperty: String?,
+        @RequestParam sortDirection: Sort.Direction,
 
         httpRequest: HttpServletRequest,
         httpResponse: HttpServletResponse
@@ -98,11 +108,12 @@ class ItemCategoryController(
         return ResponseEntity.ok(dto)
     }
 
+    @PatchMapping("/id")
     override fun patch(
         @AuthenticationPrincipal user: UserEntity?,
 
-        id: Long,
-        request: PatchItemCategoryRequest,
+        @PathVariable id: Long,
+        @RequestBody request: PatchItemCategoryRequest,
 
         httpRequest: HttpServletRequest,
         httpResponse: HttpServletResponse
@@ -126,10 +137,11 @@ class ItemCategoryController(
         return ResponseEntity.ok(dto)
     }
 
+    @DeleteMapping("/{id}")
     override fun delete(
         @AuthenticationPrincipal user: UserEntity?,
 
-        id: Long,
+        @PathVariable id: Long,
 
         httpRequest: HttpServletRequest,
         httpResponse: HttpServletResponse
