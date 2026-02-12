e71fef82c11009ae9397c2b756143d39e9cb44d6
Jan Rappe <jan.rappe2003@gmail.com>
2026-02-12 16:58:16 +0100

    Added option to save template changes & made templates editable

--- frontend/src/routes/(main)/admin/templates/[id]/+page.svelte
+++ frontend/src/routes/(main)/admin/templates/[id]/+page.svelte
@@ -1,31 +1,57 @@
 <script>
-    import {Input, Label, Textarea} from "flowbite-svelte";
+    import {Input, Label, Textarea, Button} from "flowbite-svelte";
     import {m} from '$lib/paraglide/messages.js';
     import MarkdownEditor from '$lib/components/MarkdownEditor.svelte';
+	import request from "$lib/api/api.js";
+    import { templatesPath } from "$lib/backend.js";
 
     let {data} = $props();
     let template = $derived(data.template);
+
+    let name = $derived(template.name);
+    let description = $derived(template.description);
+    let content = $derived(template.content);
+
+    let updatedTemplate = $derived({ 
+            name: name !== template.name ? name : undefined,
+            description,
+            content
+        });
+
+    async function saveTemplate() {
+
+        const response = request(templatesPath+`/${template.id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body:JSON.stringify(updatedTemplate)
+        });
+    }
 </script>
 
 <form class="flex flex-col gap-5">
-    <Label>
-        <span>{m.admin_templates_id__id_label()}</span>
-        <Input type="text" value={template.id} disabled/>
-    </Label>
-
+    <div class="flex flex-row justify-between">
+        <Label class="flex flex-col">
+            <span>{m.admin_templates_id__id_label()}</span>
+            {template.id}
+            </Label> 
+            
+        <Button class="cursor-pointer" onclick={saveTemplate}>_Save_</Button>
+    </div>
     <Label>
         <span>{m.admin_templates_id__name_label()}</span>
-        <Input type="text" value={template.name} disabled/>
+        <Input type="text" value={template.name}/>
     </Label>
 
     <Label>
         <span>{m.admin_templates_id__description_label()}</span>
-        <Textarea class="w-full" value={template.description} disabled/>
+        <Textarea class="w-full" value={template.description}/>
     </Label>
 
     <Label>
         <span>{m.admin_templates_id__content_label()}</span>
-        <MarkdownEditor class="w-full" content={template.content} disabled/>
+        <MarkdownEditor class="w-full" {content} />
     </Label>
 
     <Label>
