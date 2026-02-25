103cf5a4380d2048c134bb0bd4c61a99efbeb693
Jan Rappe <jan.rappe2003@gmail.com>
2026-02-25 10:55:32 +0100

    Added getItemBookings function

--- frontend/src/routes/(main)/user/item-bookings/+page.js
+++ frontend/src/routes/(main)/user/item-bookings/+page.js
@@ -2,15 +2,21 @@ import {ItemCategories, Items} from "$lib/api";
 import {getPage} from "$lib/utils.js";
 
 export async function load({url}) {
-    const page = await getPage(url, "/api/item-bookings")
+    const itemBookings = await getItemBookings();
     const categories = await getCategories();
     const items = await getItems();
 
-    return {
-        page: page,
-        categories: categories,
-        items: items
-    };
+    return { itemBookings, categories, items};
+}
+
+async function getCategories() {
+    const response = await ItemCategories.getCollection();
+    return response.json();
+}
+
+async function getItems() {
+    const response = await Items.getCollection();
+    return response.json();
 }
 
 async function getCategories() {
