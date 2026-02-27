5009d02f37d0543bf08f4ac5792b8fe375f6fbe8
Jan Rappe <jan.rappe2003@gmail.com>
2026-02-27 12:32:32 +0100

    First work in improved details page

--- frontend/src/lib/components/ItemDetailsPage.svelte
+++ frontend/src/lib/components/ItemDetailsPage.svelte
@@ -1,35 +1,79 @@
 <script>
     import {page} from '$app/state';
+    import { Button, Input} from 'flowbite-svelte';
+    import { PenSolid } from 'flowbite-svelte-icons';
+    import {ItemBookings} from '$lib/api';
 
     /**
      * title: Name of the item
      * content: array of objects of the form {name,value}, where name is the name of the property and value is the value of the property
      */
-    let {title, content} = $props();
+    let {title, entity} = $props();
+
+
+    let formattedEntity = $derived(entity.forEach((property) => {
+        if(Array.isArray(property)){ 
+            property.forEach((singleProperty) =>{ return {...singleProperty, editMode:false}});
+        } else {
+            return {...property, editMode:false}
+        }
+    }));
+    let endpoint = $derived(entity.endpoint);
+    let editMode = $state(false);
+
+    async function saveEntity(event, key){
+
+        const element = event.target;
+
+        let payload = {};
+
+        payload[key] = element.value;
+
+        ItemBookings.patch(page.params.id, payload);
+    }
 </script>
 
-{#snippet textWithLabel(label, content, endpoint)}
-    <div class="flex flex-col">
-        <span class="font-bold text-muted">{label}</span>
-        {#if endpoint}
-            <a href={`/manage${endpoint}`} class="text-csw">{content}</a>
-        {:else if Array.isArray(content)}
-            {#each content as item}
+{#snippet textWithLabel(property)}
+    <div class="flex-1">
+        <span class="font-bold text-muted">{property.name}</span>
+        {#if property.endpoint}
+        <div class="w-full h-11 p-2.5 border border-gray-600/70 bg-gray-700/70 rounded-lg items-center text-text">
+            <a href={`/manage${endpoint}`} class="text-orange-400">{property.content}</a></div>
+        {:else if Array.isArray(property.content)}
+            {#each property.content as item}
                 <p>{item}</p>
             {/each}
         {:else}
-            <p>{content}</p>
+            {#if property.isEditable}
+                <Input type="text" onclick={(e) => saveEntity(e, property.key)}>
+                {#snippet left()}
+                    <Button><PenSolid/></Button>
+                {/snippet}
+                </Input>
+            {:else}
+                <div class="w-full h-11 p-2.5 border opacity-70 border-gray-600 bg-gray-700 rounded-lg items-center text-text">{property.content}</div>
+            {/if}
         {/if}
     </div>
 {/snippet}
 
 <div class="flex flex-col justify-center items-center">
-    <div class="w-full flex bg-white border rounded-lg max-w-2xl border-gray-200 dark:bg-gray-800 dark:border-gray-700 shadow-md flex-col p-5 grow"
+    <div class="w-full flex flex-col bg-white border rounded-lg max-w-2xl border-gray-200 dark:bg-gray-800 dark:border-gray-700 shadow-md  p-5 grow"
          size="lg">
-        <span class="font-bold text-2xl text-center">{`${title} ID: ${page.params.id}`}</span>
+        <div class="flex flex-row justify-between">
+        <span class="font-bold text-2xl text-text text-center">{`${title} ID: ${page.params.id}`}</span>
+        <Button><PenSolid/></Button></div>
         <div class="flex flex-col gap-5">
-            {#each content as property}
-                {@render textWithLabel(property.name, property.value, property.endpoint)}
+            {#each entity.content as property}
+                {#if Array.isArray(property)}
+                <div class="flex gap-5">
+                    {#each property as singleProperty}
+                    {@render textWithLabel(singleProperty.name, singleProperty.value, singleProperty.endpoint)}
+                    {/each} 
+                </div>
+                {:else}
+                    {@render textWithLabel(property)}
+                {/if}
             {/each}
         </div>
     </div>
