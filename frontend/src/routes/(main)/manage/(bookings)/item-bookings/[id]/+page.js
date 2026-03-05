fc420e71c8fa67765ae7f84caaa3520510bbdbad
Memphis Herghelegiu <memphispc70@gmail.com>
2026-03-05 17:42:55 +0100

    implemented item bookings singular view

--- frontend/src/routes/(main)/manage/(bookings)/item-bookings/[id]/+page.js
+++ frontend/src/routes/(main)/manage/(bookings)/item-bookings/[id]/+page.js
@@ -1,11 +1,35 @@
-import {ItemBookings} from "$lib/api";
+import {ItemBookings, Items, Users} from "$lib/api";
 
 export async function load({params}) {
     const booking = await getItemBooking(params.id);
-    return {booking: booking};
+    const items = await getItems();
+    const users = await getUsers();
+    const statuses = await getItemBookingStatus();
+
+    return {
+        booking: booking,
+        items: items,
+        users: users,
+        statuses: statuses
+    };
+}
+
+async function getUsers() {
+    const response = await Users.getCollection();
+    return await response.json();
+}
+
+async function getItems() {
+    const response = await Items.getCollection();
+    return await response.json();
 }
 
 async function getItemBooking(id) {
     const response = await ItemBookings.get(id);
+    return await response.json();
+}
+
+async function getItemBookingStatus() {
+    const response = await ItemBookings.status();
     return await response.json();
 }
--- frontend/src/routes/(main)/manage/(bookings)/item-bookings/[id]/+page.svelte
+++ frontend/src/routes/(main)/manage/(bookings)/item-bookings/[id]/+page.svelte
@@ -1,20 +1,95 @@
 <script>
     import {m} from "$lib/paraglide/messages.js";
-    import {Button, ButtonGroup, Input, Spinner, Tooltip} from "flowbite-svelte";
+    import {Input, Select} from "flowbite-svelte";
     import Card from "$lib/components/Card.svelte";
-    import {
-        CloseOutline,
-        EditOutline,
-        FloppyDiskAltOutline,
-        ShareNodesOutline,
-        TrashBinOutline
-    } from "flowbite-svelte-icons";
     import Calendar from "$lib/components/Calendar.svelte";
+    import EntityPage from "$lib/components/EntityPage.svelte";
 
     let {data} = $props();
     let booking = $derived(data.booking);
 
+    const itemItems = $derived(data.items.map((item) => ({
+        value: item.id,
+        name: item.name
+    })));
+
+    const userItems = $derived(data.users.map((user) => ({
+        value: user.id,
+        name: user.username
+    })));
+
+    const statusItems = $derived(data.statuses.map((status) => ({
+        value: status,
+        name: status
+    })));
+
     let sources = [];
+
+    let items = $derived([{
+        label: m.manage_item_bookings_label_id(),
+        field: "id",
+        component: Input,
+        props: {
+            value: booking.id
+        }
+    }, {
+        label: m.manage_item_bookings_label_item(),
+        field: "itemId",
+        editable: true,
+        component: Select,
+        props: {
+            value: booking.itemId,
+            items: itemItems
+        }
+    }, {
+        label: m.manage_item_bookings_label_user(),
+        field: "userId",
+        editable: true,
+        component: Select,
+        props: {
+            value: booking.userId,
+            items: userItems
+        }
+    }, [{
+        label: m.manage_item_bookings_label_start(),
+        field: "start",
+        editable: true,
+        component: Input,
+        props: {
+            value: booking.start
+        }
+    }, {
+        label: m.manage_item_bookings_label_end(),
+        field: "end",
+        editable: true,
+        component: Input,
+        props: {
+            value: booking.end
+        }
+    }], {
+        label: m.manage_item_bookings_label_status(),
+        field: "status",
+        editable: true,
+        component: Select,
+        props: {
+            value: booking.status,
+            items: statusItems
+        }
+    }, [{
+        label: m.manage_item_bookings_label_created_at(),
+        field: "createdAt",
+        component: Input,
+        props: {
+            value: booking.createdAt
+        }
+    }, {
+        label: m.manage_item_bookings_label_updated_at(),
+        field: "updatedAt",
+        component: Input,
+        props: {
+            value: booking.updatedAt
+        }
+    }]]);
 </script>
 
 <div class="flex-1 flex flex-col 2xl:flex-row gap-5">
@@ -22,87 +97,5 @@
         <Calendar sources={sources}/>
     </Card>
 
-    <div class="flex-1 flex gap-5">
-        <Card class="grow flex flex-col gap-5">
-            <div>
-                <span>{m.manage_item_bookings_label_id()}</span>
-                <Input type="text" value={booking.id} disabled/>
-            </div>
-
-            <div>
-                <span>{m.manage_item_bookings_label_item_id()}</span>
-                <Input type="text" value={booking.itemId} disabled/>
-            </div>
-
-            <div>
-                <span>{m.manage_item_bookings_label_user_id()}</span>
-                <Input type="text" value={booking.userId} disabled/>
-            </div>
-
-            <div class="flex gap-5">
-                <div class="flex-1">
-                    <span>{m.manage_item_bookings_label_start()}</span>
-                    <Input type="text" value={(new Date(booking.start)).toLocaleString()} disabled/>
-                </div>
-                <div class="flex-1">
-                    <span>{m.manage_item_bookings_label_end()}</span>
-                    <Input type="text" value={(new Date(booking.end)).toLocaleString()} disabled/>
-                </div>
-            </div>
-
-            <div>
-                <span>{m.manage_item_bookings_label_status()}</span>
-                <Input type="text" value={booking.status} disabled/>
-            </div>
-
-            <div class="flex gap-5">
-                <div class="flex-1">
-                    <span>{m.manage_item_bookings_label_created_at()}</span>
-                    <Input type="text" value={(new Date(booking.createdAt)).toLocaleString()} disabled/>
-                </div>
-                <div class="flex-1">
-                    <span>{m.manage_item_bookings_label_updated_at()}</span>
-                    <Input type="text" value={(new Date(booking.updatedAt)).toLocaleString()} disabled/>
-                </div>
-            </div>
-        </Card>
-
-        <Card class="flex lg:flex-col gap-5 justify-between">
-            <div class="flex lg:flex-col gap-5">
-                <ButtonGroup>
-                    <Button color="alternative" class="w-full">
-                        <Tooltip trigger="click">
-                            _copied_
-                        </Tooltip>
-                        <ShareNodesOutline/>
-                    </Button>
-                </ButtonGroup>
-
-                <ButtonGroup>
-                    {#if false}
-                        <Button color="orange">
-                            {#if false}
-                                <Spinner/>
-                            {:else}
-                                <FloppyDiskAltOutline/>
-                            {/if}
-                        </Button>
-                        <Button>
-                            <CloseOutline/>
-                        </Button>
-                    {:else}
-                        <Button color="orange">
-                            <EditOutline/>
-                        </Button>
-                    {/if}
-                </ButtonGroup>
-            </div>
-
-            <ButtonGroup>
-                <Button color="red" class="w-full">
-                    <TrashBinOutline/>
-                </Button>
-            </ButtonGroup>
-        </Card>
-    </div>
+    <EntityPage items={items} onPatch={(payload) => ItemBookings.patch(bookings.id, payload)}/>
 </div>
