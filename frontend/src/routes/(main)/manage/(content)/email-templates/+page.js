2a14e07c35ca2667bb92bc73065eb88c182411d1
Z9B <t.thanh.lam.nguyen@gmail.com>
2026-03-06 00:25:34 +0100

    feat: CreateEmailTemplateModal

--- frontend/src/routes/(main)/manage/(content)/email-templates/+page.js
+++ frontend/src/routes/(main)/manage/(content)/email-templates/+page.js
@@ -1,6 +1,17 @@
-import {getPage} from "$lib/utils.js";
+import { getPage } from "$lib/utils.js";
+import { EmailTemplates } from "$lib/api";
 
-export async function load({url}) {
-        const page = await getPage(url, "/api/email-templates")
-        return {page: page};
+export async function load({ url }) {
+    const page = await getPage(url, "/api/email-templates")
+    const type = await getType();
+
+    return {
+        page: page,
+        type: type
+    };
+}
+
+async function getType() {
+    const response = await EmailTemplates.type();
+    return await response.json();
 }
