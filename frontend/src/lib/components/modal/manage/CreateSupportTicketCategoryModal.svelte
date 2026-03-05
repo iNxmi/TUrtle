6e20a5d0e6a0de214a42ab015dc13a4d4772c284
Sanel <salihovicsanel@outlook.com>
2026-03-06 00:10:20 +0100

    Support Ticket category modal

---
+++ frontend/src/lib/components/modal/manage/CreateSupportTicketCategoryModal.svelte
@@ -0,0 +1,67 @@
+<script>
+    import {Button, Heading, Hr, Input, Modal, MultiSelect, Select, Spinner} from "flowbite-svelte";
+    import {m} from "$lib/paraglide/messages.js";
+    import {Roles, SupportTicketCategories} from "$lib/api";
+    import {onMount} from "svelte";
+    import {invalidateAll} from "$app/navigation";
+    import {Users} from "$lib/api";
+    import PasswordInput from "$lib/components/PasswordInput.svelte";
+
+    let {
+        open = $bindable(false),
+    } = $props();
+
+    let input = $state({
+        name: ""
+    })
+
+    let loading = $state(false);
+    let error = $state("");
+
+    async function submit(event) {
+        event.preventDefault()
+        error = "";
+
+        const payload = $state.snapshot(input);
+
+        loading = true;
+        const response = await SupportTicketCategories.create(payload);
+        loading = false;
+
+        if (response.status !== 201) {
+            const json = await response.json();
+            error = json.message;
+            return;
+        }
+
+        await invalidateAll();
+        open = false;
+    }
+</script>
+
+<Modal form bind:open={open} outsideclose={false}>
+    <form onsubmit={submit} class="flex flex-col gap-5">
+        <Heading tag="h3" class="text-center">
+            _CreateSupportTicketCategory_
+        </Heading>
+
+        <Hr class="m-0 p-0"/>
+
+        <div>
+            <div>_CreateTicketCategoryName_</div>
+            <Input name="name" type="text" bind:value={input.name} required/>
+        </div>
+
+        {#if error?.trim()}
+            <div class="text-red-400 text-justify">{error}</div>
+        {/if}
+
+        <Button name="button_submit" type="submit">
+            {#if loading === true}
+                <Spinner size="5"/>
+            {:else}
+                _CreateSupportTicketCategory_
+            {/if}
+        </Button>
+    </form>
+</Modal>
