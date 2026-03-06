5014eaa0ac1d9e9496fce12cd64558cf93f2fa44
Jan Rappe <jan.rappe2003@gmail.com>
2026-03-06 23:34:22 +0100

    Created Button component

---
+++ frontend/src/lib/components/Button.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { Button } from "flowbite-svelte"
+    let {class: className = "",
+        onclick = () =>{},
+        children,
+        ...rest
+    } = $props();
+</script>
+<Button class={`bg-orange-400! dark:bg-orange-600! text-white dark:text-white! cursor-pointer ${className}`} {onclick} {...rest}>
+    {@render children?.()}
+</Button>
