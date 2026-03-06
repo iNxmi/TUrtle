f15ec865e738eac47eb1370dddde404c2a7af6d8
Z9B <t.thanh.lam.nguyen@gmail.com>
2026-03-06 01:23:35 +0100

    feat: CreateRoleModal

--- frontend/src/routes/(main)/manage/(user-management)/roles/+page.js
+++ frontend/src/routes/(main)/manage/(user-management)/roles/+page.js
@@ -1,6 +1,24 @@
 import {getPage} from "$lib/utils.js";
+import {Roles, Permissions} from "$lib/api";
 
 export async function load({url}) {
     const page = await getPage(url, "/api/roles")
-    return {page: page};
+    const type = await getType();
+    const permissions = await getPermissions();
+
+    return {
+        page: page,
+        type: type,
+        permissions: permissions
+    };
+}
+
+async function getType() {
+    const response = await Roles.type();
+    return await response.json();
+}
+
+async function getPermissions() {
+    const response = await Permissions.permission();
+    return await response.json();
 }
